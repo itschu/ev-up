890cd01d32063cf7d75ca45c6f37433e85729726
itschu <palmerbideconcepts@gmail.com>
2022-12-13 12:46:11 +0100

    working on font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./pages/**/*.{js,ts,jsx,tsx}", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				body: ["Nunito"],
+			},
+		},
 	},
 	plugins: [],
 };
